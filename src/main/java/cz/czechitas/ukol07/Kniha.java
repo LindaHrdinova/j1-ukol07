fe09cb003b572e009dc26931291d4e6f32df2d3d
Linda Hrdinová <hrlin@seznam.cz>
2024-03-30 00:43:27 +0100

    Class knihy

---
+++ src/main/java/cz/czechitas/ukol07/Kniha.java
@@ -0,0 +1,31 @@
+package cz.czechitas.ukol07;
+
+public class Kniha {
+    private String autor;
+    private String nazev;
+    private Number rokVydani;
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getNazev() {
+        return nazev;
+    }
+
+    public void setNazev(String nazev) {
+        this.nazev = nazev;
+    }
+
+    public Number getRokVydani() {
+        return rokVydani;
+    }
+
+    public void setRokVydani(Number rokVydani) {
+        this.rokVydani = rokVydani;
+    }
+}
